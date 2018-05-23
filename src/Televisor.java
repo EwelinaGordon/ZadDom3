fc6d9b1fb620cdd013b33a533dffa56c12c04ccc
EwelinaGordon <ewelina.gordon@gmail.com>
2018-05-23 23:57:55 +0200

    Zadanie 1

---
+++ src/Televisor.java
@@ -0,0 +1,19 @@
+public class Televisor {
+
+    boolean isOn;
+
+    boolean getInfo(){
+        return isOn;
+    }
+
+    void turnOn() {
+        isOn = true;
+
+    }
+
+    void turnOff() {
+        isOn = false;
+    }
+
+}
+
